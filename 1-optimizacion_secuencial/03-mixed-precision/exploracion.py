e205c3a0ae8b103a359318ad5b47c95ac11ddf24
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2020-12-15 16:31:38 -0300

    0. proyecto inicial. 1. README.md modificado 2. 1-optimizacion_secuencial/ scripts para explorar flags en SoA, AoS y mixed-precision 3. 2-vectorizacion/autovectorizacion/ para ver que loops autovectoriza el compilador y que hacer a mano

---
+++ 1-optimizacion_secuencial/03-mixed-precision/exploracion.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+import fileinput, os
+import time
+
+idx = 0
+for line in fileinput.input():
+    os.system('make clean')
+
+    # recorre los distintos flags que tira $ cat flaglist.txt
+    os.system('make {} > out{:02d}.txt'.format(line.rstrip(), idx))
+
+    # header de out{:02d}.txt 
+    os.system('echo "" >> out{:02d}.txt'.format(idx))
+    os.system('echo "############# runnig with perf stat" >> out{:02d}.txt'.format(idx))
+    os.system('echo "" >> out{:02d}.txt'.format(idx))
+
+    # perf stat
+    os.system('perf stat -r 30 ./tiny_md >> out{:02d}.txt'.format(idx))
+    idx+=1
