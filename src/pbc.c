2149262e5f02b457c37e7ba24a9f49036f84272b
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-03-04 10:04:02 -0300

    pbc: if -> while (peligroso mientras se modifica el código)

--- src/pbc.c
+++ src/pbc.c
@@ -7,9 +7,9 @@ void pbc(double *rxyz, const double box_size){
 
     for (int i = 0; i < N; i++){
         for (int j = 0; j < 3; j++){
-            if (rxyz[j*N + i] <= 0.0)
+            while (rxyz[j*N + i] <= 0.0)
                 rxyz[j*N + i] += box_size;
-            if (rxyz[j*N + i] > box_size)
+            while (rxyz[j*N + i] > box_size)
                 rxyz[j*N + i] -= box_size;
         }
     }
